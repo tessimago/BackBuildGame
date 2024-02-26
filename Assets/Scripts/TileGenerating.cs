b18be56e1b3f7ba6d01c70f1b10dd8b9eb4e9aea
Iago Paulo <tessimago@gmail.com>
2024-02-26 21:46:11 +0000

    Empty project created
    and also starting to create Tilemap

---
+++ Assets/Scripts/TileGenerating.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileGenerating : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
