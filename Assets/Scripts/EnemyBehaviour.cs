e39f05c40f7b0ae58aa05f84d9c0eeda89e785ab
Iago Paulo <tessimago@gmail.com>
2024-03-02 17:07:40 +0000

    Enemys attack, and are now animated
    Enemys attack with  sec cooldown for each attack, they are pushed back as well for each rush attack, there was a slight change on player movment as well so I can knockback the player.
    And I animated the enemys with a 3 sprite animation :)

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,27 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     GameObject player;
+    Rigidbody2D rb;
+
+    public bool canAttack;
     // Start is called before the first frame update
     void Start()
     {
+        canAttack = true;
         player = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        lookToPlayer();
+        if(canAttack)
+            walkToPlayer();
+    }
+    void walkToPlayer(){
+        transform.position = Vector2.MoveTowards(
+                                transform.position, player.transform.position, 5 * Time.deltaTime);
+    }
+    void lookToPlayer()
+    {
         // Some math just to look at the player
         float m = (player.transform.position.y - transform.position.y) 
                                             /
-                  (player.transform.position.x - transform.position.x);
+                    (player.transform.position.x - transform.position.x);
         float angle = Mathf.Atan(m) * Mathf.Rad2Deg - 90;
         if(player.transform.position.x < transform.position.x)
             angle += 180;
         transform.rotation = Quaternion.Euler(0, 0, angle);
-
+    }
+    IEnumerator attacked(){
+        canAttack = false;
+        yield return new WaitForSeconds(3);
+        canAttack = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            var forceDir = (transform.position - collision.transform.position).normalized;
+            var force = new Vector2(forceDir.x, forceDir.y) * 5;
+            rb.AddForce(force, ForceMode2D.Impulse);
+            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerRb.AddForce(-force, ForceMode2D.Impulse);
+            StartCoroutine(attacked());
+        }
     }
 }
--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -6,34 +6,50 @@ public class PlayerMov : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed = 5f;
+    float realSpeed;
+    float x;
+    float y;
     // Start is called before the first frame update
     void Start()
     {
+        x = 0;
+        y = 0;
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Player input axis
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-        
-        //Move the player
-        rb.velocity = new Vector2(x, y) * speed;
+        realSpeed = speed * rb.drag;
 
+        //Player input axis
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        speedControl();
+        lookToMouse();
+        Debug.Log("Speed: "+ rb.velocity.magnitude);
+    }
+
+    void lookToMouse(){
         // Rotate looking at the mouse
         Vector3 mousePos = Input.mousePosition;
         //Screen to world
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
         mousePos.z = 0;
         transform.up = mousePos - transform.position;
-
-
     }
 
     // All Physic related stuff eventually
     void FixedUpdate() {
+        //Move the player
+        if(x != 0 || y != 0)
+            rb.AddForce(new Vector2(x,y) * realSpeed, ForceMode2D.Force);
+        
+    }
 
+    void speedControl(){
+        if(rb.velocity.magnitude > speed)
+            rb.velocity = rb.velocity.normalized * speed;
+        
     }
 }
